db7a047363ae8d21657203247c81fb740f5c981b
tehwenyi <teh.wenyi22@gmail.com>
2023-05-04 16:27:54 +0800

    reformat so that is easier to comment out unneeded params; make script workable again after changes in some scripts

--- helper/run_coco_to_yolo.sh
+++ helper/run_coco_to_yolo.sh
@@ -2,9 +2,23 @@ IMG_FOLDER='/path/to/img/folder'
 JSON_PATH='/path/to/annotations/json'
 OUTPUT_PATH='path/to/output/non-tiled/folder'
 OUTPUT_TILED_PATH='/path/to/output/tiled/folder'
+FALSEPATH='/path/to/store/empty/tiled/images'
 
-python coco_to_yolo.py $IMG_FOLDER $JSON_PATH $OUTPUT_PATH
-# Optional: Further split into train & val folders
-python split_train_val.py $OUTPUT_PATH $OUTPUT_PATH # Remember to delete "images" and "labels" from $OUTPUT_PATH
+python coco_to_yolo.py \
+  $IMG_FOLDER \
+  $JSON_PATH \
+  $OUTPUT_PATH
+
+# Optional: Further split into train, val & test folders
+# Remember to delete "images" and "labels" from $OUTPUT_PATH
+python split_train_val_test.py \
+  $OUTPUT_PATH \
+  $OUTPUT_PATH \
+  --test True
+
 # Optional: Tiling
-# python tiling.py $OUTPUT_PATH $OUTPUT_TILED_PATH --size 640
+python tiling.py \
+  $OUTPUT_PATH \
+  $OUTPUT_TILED_PATH \
+  --falsepath $FALSEPATH \
+  --size 640
